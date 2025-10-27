3711b4e7f16109a32648a6689a651929dd8ca2c3
YeyeJames <144203282+YeyeJames@users.noreply.github.com>
2025-10-27 10:30:18 +0800

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,18 +1,17 @@
 // storage.js
-export const Storage = {
+const Storage = {
   loadProfile() {
     return JSON.parse(localStorage.getItem("profile") || "{}");
   },
-
   saveProfile(profile) {
     localStorage.setItem("profile", JSON.stringify(profile));
   },
-
   loadProgress(week) {
     return JSON.parse(localStorage.getItem(`progress_week_${week}`) || "{}");
   },
-
   saveProgress(week, data) {
     localStorage.setItem(`progress_week_${week}`, JSON.stringify(data));
   },
 };
+
+window.Storage = Storage;
